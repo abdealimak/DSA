fb2cc3791375836357383972d1ae47636e2c40aa
Abdeali Makda <abdealimac@gmail.com>
2026-01-26 20:11:19 +0530

    Add files via upload

---
+++ patterns/star_patterns/invrat.cpp
@@ -0,0 +1,19 @@
+/*
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i = 1; i <= 5; i++){
+        for(int j = 0; j < 5 - i + 1; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
